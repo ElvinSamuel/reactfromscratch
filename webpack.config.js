110caebd121ff99285e98d4ce6658f080075b400
Elvin Samuel <elvinsamuel1@gmail.com>
2018-07-27 20:29:36 +0000

    Adding to git, since I'm clearing this workspace

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,38 @@ const path    = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: "./src/index.js",
-	mode: "development",
-	module: {
-		rules: [
+	"entry": [
+			'webpack-dev-server/client?https://0.0.0.0:8080',
+			'webpack/hot/only-dev-server',
+			'src/index.js'
+		],
+	"mode": "development",
+	"module": {
+		"rules": [
 			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules|bower_components)/,
+				"test": /\.(js|jsx)$/,
+				"exclude": /(node_modules|bower_components)/,
 		        	loader: 'babel-loader',
 	        		options: { presets: ['env'] }	       
 				},
 				{
-				test: /\.css$/,
-				use: [ 'style-loader', 'css-loader']
+				"test": /\.css$/,
+				"use": [ 'style-loader', 'css-loader']
 				}
 		]
 	},
-	resolve: { extensions: ['*', '.js', '.jsx'] },
-	output:  {
-		path: path.resolve(__dirname, "dist/"),
-		publicPath: "/dist",
-		filename: "bundle.js"
+	"resolve": { "extensions": ['*', '.js', '.jsx'] },
+	"output":  {
+		"path": path.resolve(__dirname, "dist/"),
+		"publicPath": "/dist",
+		"filename": "bundle.js"
 	},
-	devServer: {
-		contentBase: path.join(__dirname, "public/"),
-		port: 3000,
-		publicPath: "http://localhost:3000/dist/",
-		hotOnly: true
+	"devServer": {
+		"contentBase": path.join(__dirname, "public/"),
+		"port": 3000,
+		"publicPath": "http://localhost:3000/dist/",
+		"hotOnly": true
 	},
-	plugins: [ new webpack.HotModuleReplacementPlugin() ]
+	"plugins": [ new webpack.HotModuleReplacementPlugin() ]
 };
 
